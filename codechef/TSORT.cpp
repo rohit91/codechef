7724279b3e7e63fd6aa1e60575107a718e7b90ad
Rohit Kumar Singh <rohit91.2008@gmail.com>
2015-12-22 01:39:34 +0530

    Removed combine() function from C++ implementation of Merge Sort and moved it inline
     On branch master
     Changes to be committed:
    	modified:   TSORT.cpp

--- codechef/TSORT.cpp
+++ codechef/TSORT.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 
 void mergesrt(std::vector<unsigned long long>&, unsigned long long, unsigned long long);
-void combine(std::vector<unsigned long long>&, unsigned long long, unsigned long long);
 
 int main()
 {
@@ -41,33 +40,19 @@ void mergesrt(std::vector<unsigned long long>& a, unsigned long long lower, unsi
     if (lower == upper -1)
         return;
         
-    unsigned long long int mid = (lower + upper)/2;
-        
+    unsigned long long mid = (lower + upper)/2;
+    
+    // Recursion
     mergesrt(a, lower, mid);
     mergesrt(a, mid, upper);
     
-    combine(a, lower, upper);
-}
-
-
-
-/* combine Function
- *      Merges to sorted arrays into a final sorted array, in same array
- * 
- * Parameters ::
- *      a    : Pointer to original array
- *      lower: Lower index of array. Is inclusive
- *      upper: Upper bound of array. Is exclusive, that is valid array
- *             index is only till upper-1
- */
-void combine(std::vector<unsigned long long>& a, unsigned long long lower, unsigned long long upper)
-{
-    unsigned long long i=0, j=0, k=0, mid=0;
-    mid = (lower + upper)/2;
     
+    /*  Vector Merge Code starts below */
+    
+    unsigned long long i=0, j=0, k=0;
     std::vector<unsigned long long> result(upper-lower);
     
-    // Merge 
+    // Compare and add smaller value to temporary vector
     for(i=lower, j=mid, k=0; i<mid && j<upper; )
     {
         if (a[i] < a[j])
@@ -79,11 +64,13 @@ void combine(std::vector<unsigned long long>& a, unsigned long long lower, unsig
     // Copy remaining elements
     while(i<mid)
         result[k++] = a[i++];
-    
+
     while(j<upper)
         result[k++] = a[j++];
     
     // Copy combined vector onto original vector
     for(k=0; k<(upper-lower); k++)
         a[lower+k] = result[k];
+    
+    /* Merge code ends */
 }

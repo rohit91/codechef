f1220ee3b048e377872f807742f1a4526ea830f0
Rohit Kumar Singh <rohit91.2008@gmail.com>
2016-01-29 22:43:05 +0530

    Python solution for REMISS problem. Solution based on Set theory / Venn Diagram   Changes to be committed: 	new file:   REMISS.py

---
+++ codechef/REMISS.py
@@ -0,0 +1,13 @@
+# Mathematics: 
+#   (AuB = A union B; AiB = A intersection B) 
+#   AuB >= Max(A, B)
+#   Also since AiB >=0 (Given in problem), 
+#        therefore A+B-AuB >=0, 
+#        hence AuB <= A+B
+#   Range is: max(A,B) <= AuB <= A+B
+
+T = int(raw_input())
+
+for i in range(T):
+    nums = map(int, raw_input().split())
+    print '{} {}'.format(max(nums), sum(nums))
